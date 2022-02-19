0df932d2d7d131ca980ca2009f04e3ef39fefb1a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2022-02-19 00:51:42 +0000

    update stack to include stack name

--- infrastructure/AppServiceStack.cs
+++ infrastructure/AppServiceStack.cs
@@ -7,20 +7,23 @@ using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Sql;
 using Pulumi.AzureNative.Web;
 using Pulumi.AzureNative.Web.Inputs;
+using Deployment = Pulumi.Deployment;
 using SkuArgs = Pulumi.AzureNative.Sql.Inputs.SkuArgs;
 
 class AppServiceStack : Stack
 {
-    [Output]
-    public Output<string> Endpoint { get; set; }
-    
+    [Output] public Output<string> Endpoint { get; set; }
+
     public AppServiceStack()
     {
-        var currentUser = Output.Create(GetClientConfig.InvokeAsync());
-    
-        var resourceGroup = new ResourceGroup("ss-dotnet-api-rg");
+        var stackName = Deployment.Instance.StackName;
+        var appName = "ss-dotnet-api";
+        var resourceGroup = new ResourceGroup($"{appName}-rg-{stackName}", new ResourceGroupArgs()
+        {
+            ResourceGroupName = $"{appName}-rg-{stackName}"
+        });
 
-        var appServicePlan = new AppServicePlan("ss-dotnet-api-asp", new AppServicePlanArgs
+        var appServicePlan = new AppServicePlan($"{appName}-asp-{stackName}", new AppServicePlanArgs
         {
             ResourceGroupName = resourceGroup.Name,
             Kind = "App",
@@ -29,70 +32,80 @@ class AppServiceStack : Stack
                 Tier = "Free",
                 Name = "F1",
             },
+            Name = $"{appName}-asp-{stackName}"
         });
 
-        var appInsights = new Component("ss-dotnet-api-appInsights", new ComponentArgs
+        var appInsights = new Component($"{appName}-appInsights-{stackName}", new ComponentArgs
         {
             ApplicationType = "web",
             Kind = "web",
             ResourceGroupName = resourceGroup.Name,
+            ResourceName = $"{appName}-appInsights-{stackName}"
         });
 
         var config = new Config();
         var username = config.Get("sqlAdmin") ?? "pulumi";
         var password = config.RequireSecret("sqlPassword");
-        var sqlServer = new Server("ss-dotnet-api-sqlserver", new ServerArgs
+        var sqlServer = new Server($"{appName}-sqlserver-{stackName}", new ServerArgs
         {
             ResourceGroupName = resourceGroup.Name,
             AdministratorLogin = username,
             AdministratorLoginPassword = password,
             Version = "12.0",
+            ServerName = $"{appName}-sqlserver-{stackName}"
         });
 
-        var database = new Database("ss-dotnet-api-db", new DatabaseArgs
+        var database = new Database($"{appName}-db-{stackName}", new DatabaseArgs
         {
             ResourceGroupName = resourceGroup.Name,
             ServerName = sqlServer.Name,
             Sku = new SkuArgs
             {
-                Name =  "S0"
-            }
+                Name = "S0"
+            },
+            DatabaseName = $"{appName}-db-{stackName}"
         });
 
-        var app = new WebApp("ss-dotnet-api", new WebAppArgs
+        var app = new WebApp($"{appName}-app-{stackName}", new WebAppArgs
         {
+            Name = $"{appName}-app-{stackName}",
             ResourceGroupName = resourceGroup.Name,
             ServerFarmId = appServicePlan.Id,
             SiteConfig = new SiteConfigArgs
             {
-                AppSettings = {
-                    new NameValuePairArgs{
+                AppSettings =
+                {
+                    new NameValuePairArgs
+                    {
                         Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
                         Value = appInsights.InstrumentationKey
                     },
-                    new NameValuePairArgs{
+                    new NameValuePairArgs
+                    {
                         Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
                         Value = appInsights.InstrumentationKey.Apply(key => $"InstrumentationKey={key}"),
                     },
-                    new NameValuePairArgs{
+                    new NameValuePairArgs
+                    {
                         Name = "ApplicationInsightsAgent_EXTENSION_VERSION",
                         Value = "~2",
                     },
                 },
-                ConnectionStrings = {
+                ConnectionStrings =
+                {
                     new ConnStringInfoArgs
                     {
                         Name = "db",
                         Type = ConnectionStringType.SQLAzure,
                         ConnectionString = Output.Tuple(sqlServer.Name, database.Name, password).Apply(t =>
                         {
-                            (string server, string dbName, string pwd) = t;
+                            var (server, dbName, pwd) = t;
                             return
                                 $"Server= tcp:{server}.database.windows.net;initial catalog={dbName};userID={username};password={pwd};Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;";
                         }),
                     },
                 },
-            }
+            },
         });
 
         Endpoint = app.DefaultHostName;
