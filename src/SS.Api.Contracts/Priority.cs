be1d6c1512bc2785eb6ac7e8adc242c20341b7cc
Shaed Parkar <shaed.parkar@slimsolutions.company>
2022-03-17 01:22:32 +0000

    Add missing XML docs for Priority.cs

--- src/SS.Api.Contracts/Priority.cs
+++ src/SS.Api.Contracts/Priority.cs
@@ -5,8 +5,20 @@ namespace SS;
 /// </summary>
 public enum Priority
 {
+    /// <summary>
+    ///     Unknown priority level
+    /// </summary>
     Unknown = 0,
+    /// <summary>
+    ///     Low-level priority level
+    /// </summary>
     Low = 1,
+    /// <summary>
+    ///     Medium-level priority level
+    /// </summary>
     Medium = 2,
+    /// <summary>
+    ///     High-level priority level
+    /// </summary>
     High = 3
 }
